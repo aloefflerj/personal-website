1e477ca6c32242b4f51d1b18bfddecc980f991ce
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-08-28 01:01:27 -0300

    fix: fixes page height to entire screen

--- src/pages/CategoryPageLayout.jsx
+++ src/pages/CategoryPageLayout.jsx
@@ -10,7 +10,7 @@ const Page = styled.div`
         'sidebar main'
         'sidebar main';
     width: 100%;
-    height: 100%;
+    height: 100vh;
     background-color: ${(props) => props.$bgColor};
 `;
 
