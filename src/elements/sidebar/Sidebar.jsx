0897539bbbb2f1698b5b1ce4c62d1519a36172d0
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-08-28 00:58:36 -0300

    reafactor: changes sidebar positioning

--- src/elements/sidebar/Sidebar.jsx
+++ src/elements/sidebar/Sidebar.jsx
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 
 const Bar = styled.aside`
     grid-area: sidebar;
-    padding: 0 12px;
+    padding: 0 24px;
     display: flex;
     flex-direction: column;
-    margin: 3px 0 3px 3px;
+    margin: 0 0 0 9px;
+    position: fixed;
     /* width: 100%; */
     height: calc(100vh - 6px);
     background-color: ${(props) => props.$category.darkColor};
@@ -14,11 +15,10 @@ const Bar = styled.aside`
         inset 3px -3px ${(props) => props.$category.mediumColor},
         inset -3px 3px ${(props) => props.$category.mediumColor},
         0px -6px 0px 0px ${(props) => props.$category.darkerColor},
-        6px 0px 0px 0px ${(props) => props.$category.darkerColor},
+        3px 0px 0px 0px ${(props) => props.$category.darkerColor},
         0px 6px 0px 0px ${(props) => props.$category.darkerColor},
-        -6px 0px 0px 0px ${(props) => props.$category.darkerColor},
+        -3px 0px 0px 0px ${(props) => props.$category.darkerColor},
         0px -6px 0px 0px ${(props) => props.$category.darkerColor};
-    border-right: 3px solid ${(props) => props.$category.darkerColor};
 `;
 
 export function Sidebar({ children, category }) {
