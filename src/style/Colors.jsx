029a12f194fe13a1bf24363d56673deb0d09297d
Anderson Job Loeffler <aloeffler.j@gmail.com>
2023-10-15 15:24:02 -0300

    :sparkles: fix: fixes 'game' design coloring

--- src/style/Colors.jsx
+++ src/style/Colors.jsx
@@ -2,7 +2,7 @@ export const CategoryMediumColors = {
     blank: '#262a32',
     code: '#46467c',
     drawings: '#63a063',
-    game: '#5b1c51',
+    game: '#aa5a95',
     music: '#796027;',
     worldbuilding: '#da7649',
 };
@@ -11,7 +11,7 @@ export const CategoryLightColors = {
     blank: '#444c55',
     code: '#7180b6',
     drawings: '#83bb83',
-    game: '#79446c',
+    game: '#d08dbf',
     music: '#917359',
     worldbuilding: '#e6a271',
 };
@@ -20,7 +20,7 @@ export const CategoryLighterColors = {
     blank: '#7b858f',
     code: '#9ca7c7',
     drawings: '#b2d6b7',
-    game: '#ac7da4',
+    game: '#e6c0df',
     music: '#af9a87',
     worldbuilding: '#efd7c3',
 };
@@ -29,7 +29,7 @@ export const CategoryDarkColors = {
     blank: '#1c1d27',
     code: '#251f42',
     drawings: '#14302b',
-    game: '#290c31',
+    game: '#572b5f',
     music: '#3f4021',
     worldbuilding: '#6d2f1d',
 };
@@ -38,7 +38,7 @@ export const CategoryDarkerColors = {
     blank: '#070c0f',
     code: '#110f27',
     drawings: '#081816',
-    game: '#13051b',
+    game: '#321937',
     music: '#1a1b08',
     worldbuilding: '#3b1612',
 };
@@ -47,7 +47,7 @@ export const CategoryBGColors = {
     blank: CategoryDarkColors.blank,
     code: '#34305f',
     drawings: '#1f4b3e',
-    game: '#551e57',
+    game: '#65386e',
     music: '#4d4113',
     worldbuilding: '#773825',
 };
