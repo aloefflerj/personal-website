8af753f7a6a754cd80e7ceeb32c95f6eeedff790
Anderson Job Loeffler <aloeffler.j@gmail.com>
2024-01-14 18:26:16 -0300

    :bug: fix: removes text decoration from nav link

--- src/components/categories/CategoryNavLinkItem.jsx
+++ src/components/categories/CategoryNavLinkItem.jsx
@@ -3,17 +3,21 @@ import { CategoryOption } from './CategoryOption';
 import PropTypes from 'prop-types';
 import { Category } from '../../categories/Categories';
 import { If } from '../If';
+import styled from 'styled-components';
 
+const Link = styled(NavLink)`
+    text-decoration: none;
+`;
 export function CategoryNavLinkItem({ category, button = null }) {
     return (
-        <NavLink to={category.categoryKey}>
+        <Link to={category.categoryKey}>
             <If is={button === null}>
                 <CategoryOption buttonCategory={category}>
                     {category.title}
                 </CategoryOption>
             </If>
             <If is={button !== null}>{button}</If>
-        </NavLink>
+        </Link>
     );
 }
 
