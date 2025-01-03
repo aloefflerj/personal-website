9ebeba22245702b1f2688c8e963c86a6c06c986d
Anderson Job Loeffler <aloeffler.j@gmail.com>
2025-01-03 15:24:04 -0300

    feat: renders mermaid content dinamically

--- src/components/codeblock/CodeBlock.jsx
+++ src/components/codeblock/CodeBlock.jsx
@@ -1,19 +1,20 @@
 import PropTypes from 'prop-types';
 import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
-// import { paraisoDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import { CategoryCodeBlockStyle } from './CategoryCodeBlockStyle';
 import { useCategoryContext } from '../../hooks/useCategoryContext';
+import { Mermaid } from '../mermaid/Mermaid';
 
-export default function CodeBlock({ language = null, children, inline }) {
+export default function CodeBlock({ className, children, inline }) {
     const { category } = useCategoryContext();
 
     return (
-        <SyntaxHighlighter
-            language={language ?? 'php'}
-            style={CategoryCodeBlockStyle(category, inline)}
-        >
-            {children}
-        </SyntaxHighlighter>
+        className === 'language-mermaid' ?
+            <Mermaid chart={children} /> :
+            <SyntaxHighlighter
+                language={className ?? 'language-php'}
+                style={CategoryCodeBlockStyle(category, inline)}
+            >
+            </SyntaxHighlighter>
     );
 }
 
---
+++ src/components/mermaid/Mermaid.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from "react";
+import mermaid from "mermaid";
+
+export function Mermaid({ chart }) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    mermaid.initialize({ startOnLoad: false, fontFamily: 'pixel' });
+
+    const renderDiagram = async () => {
+      if (containerRef.current && chart) {
+        const content = chart[0];
+
+        if (typeof content === 'string' && content.trim()) {
+          const { svg } = await mermaid.render('mermaidDiagram', content.trim());
+          containerRef.current.innerHTML = svg;
+        }
+      }
+    }
+
+    renderDiagram();
+
+  }, [chart]);
+
+  return <div ref={containerRef} />;
+};
